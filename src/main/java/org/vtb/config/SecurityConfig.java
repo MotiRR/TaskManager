b3dd49f97b9b0f54ac76e86c89d1e51d8ad44ef0
MotiRR <motigullin.ra@gmail.com>
2020-09-07 18:43:38 +0300

    update AuthController, AuthResponse, ProjectController, SecurityConfig, UserService

--- src/main/java/org/vtb/config/SecurityConfig.java
+++ src/main/java/org/vtb/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/api/**").hasRole("USER")
-                .antMatchers("/register", "/auth").permitAll()
+                .antMatchers("/register", "/auth", "/login").permitAll()
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
--- src/main/java/org/vtb/controller/ProjectController.java
+++ src/main/java/org/vtb/controller/ProjectController.java
@@ -3,9 +3,11 @@ package org.vtb.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.vtb.entity.Project;
+import org.vtb.entity.Task;
 import org.vtb.service.ProjectService;
 
 import java.util.List;
@@ -19,6 +21,11 @@ public class ProjectController {
     @Autowired
     public void setService(ProjectService service) {
         this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public Project getProjectById(@PathVariable Long id) {
+        return service.findById(id);
     }
 
     @GetMapping
